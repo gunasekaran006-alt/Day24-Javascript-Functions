55cbbfd43db04add3dfdabdaa3196ea4edec499f
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-21 22:36:14 +0530

    updates

--- script.js
+++ script.js
@@ -8,37 +8,63 @@
 // functionName() - function callback with arguments
 
 // Function Syntax
+// type:1
 function welcome(){
     console.log("Welcome Learners");
 }
-
 welcome();
+// type:2
+function welcome(name){
+    console.log(`Welcome ${name}`); // Welcome Guna
+}
+welcome("Guna");
 
-// Function with Conditions
+
+// // Function with Conditions (Hardcoding method)
+// Type:1
 function checkAge(){
     let age = 18;
-
     if(age >= 18){
+        console.log("Eligible"); 
+    } else{
+        console.log("Not Eligible"); 
+    }
+}
+checkAge(); // Eligible
+// Type:2
+function checkAge1(age1){
+    if(age1 >= 18) {
         console.log("Eligible");
-    } else {
+    }else {
         console.log("Not Eligible");
     }
 }
+checkAge1(18); // Eligible
+checkAge1(15); // Not Eligible
 
-checkAge();
 
 // Function parameters and arguments
-let a = 5;
-let b = 10;
-
+// Type:1
+let a = 10;
+let b = 15;
 let c = 20;
 let d = 25;
-
-function add(num1, num2, num3, num4){
-    console.log(num1 + num2 + num3 + num4);
+function add(num1,num2,num3,num4){
+    console.log(num1+num2+num3+num4);
 }
+add(a,b,c,d); // 70
+// Type:2 ( Rest Parameter-more nbr adding purpose)
+function addEverything(...numbers){
+    let total = 0;
+    // for loop modern method of -> for...of 
+for(let num of numbers){
+total += num;
+}
+console.log(total);
+}
+addEverything(10,15,20,25); // 70
 
-add(a, b, c, d);
+
 
 // return statement example
 
